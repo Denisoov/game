fceca17d62b7870b4da11fd46865913bb602f01f
kadonomaro <denis.afer@gmail.com>
2024-10-17 02:57:03 +0700

    mini-game-1: add clicker game draft

--- src/scenes/games/clicker.js
+++ src/scenes/games/clicker.js
@@ -3,12 +3,17 @@ export default class ClickerGameScene extends Phaser.Scene {
 		super({ key: 'ClickerGameScene' });
 		this.score = 0;
 		this.coins = null;
+		this.coinEvent = null;
 		this.timeLeft = 30;
 		this.timerText = null;
+		this.timerEvent = null;
+		this.sprites = ['clicker/sushi', 'clicker/sushi-caviar'];
+		this.scoreAmounts = [1, 5];
 	}
 
 	preload() {
-		this.load.image('clicker/coin', './assets/japan/sushi.png')
+		this.load.image('clicker/sushi', './assets/japan/sushi.png')
+		this.load.image('clicker/sushi-caviar', './assets/japan/sushi-caviar.png')
 	}
 
 	create() {
@@ -19,7 +24,7 @@ export default class ClickerGameScene extends Phaser.Scene {
 		// Создание группы монет
 		this.coins = this.physics.add.group();
 
-		this.time.addEvent({
+		this.coinEvent = this.time.addEvent({
 			delay: 500,
 			callback: this.createCoin,
 			callbackScope: this,
@@ -33,7 +38,7 @@ export default class ClickerGameScene extends Phaser.Scene {
 			loop: true
 		});
 
-		this.time.addEvent({
+		this.timerEvent = this.time.addEvent({
 			delay: 1000,
 			callback: this.updateTimer,
 			callbackScope: this,
@@ -44,9 +49,31 @@ export default class ClickerGameScene extends Phaser.Scene {
 	}
 
 	createCoin() {
-		const x = Phaser.Math.Between(0, this.sys.game.config.width);
-		const y = Phaser.Math.Between(0, this.sys.game.config.height);
-		const coin = this.coins.create(x, y, 'clicker/coin');
+		let x, y;
+		let validPosition = false;
+
+		// Генерация координат до тех пор, пока не найдём подходящие
+		while (!validPosition) {
+			x = Phaser.Math.Between(50, this.sys.game.config.width - 50); // Учитываем ширину монеты
+			y = Phaser.Math.Between(100, this.sys.game.config.height - 50); // Учитываем высоту монеты
+
+			// Проверяем, не персекается ли новая монета с уже существующими
+			validPosition = true;
+			this.coins.children.iterate((coin) => {
+				if (coin && Phaser.Geom.Intersects.RectangleToRectangle(
+					new Phaser.Geom.Rectangle(x - 25, y - 25, 50, 50), // Размеры монеты
+					coin.getBounds()
+				)) {
+					validPosition = false; // Если пересекается, ищем новые координаты
+				}
+			});
+		}
+
+		const spriteIndex = Phaser.Math.Between(0, this.sprites.length - 1);
+		const scoreAmount = this.scoreAmounts[spriteIndex];
+
+		const coin = this.coins.create(x, y, this.sprites[spriteIndex]);
+		coin.setData('score', scoreAmount);
 
 		coin.setAlpha(0);
 
@@ -82,22 +109,45 @@ export default class ClickerGameScene extends Phaser.Scene {
 	}
 
 	endGame() {
-		this.scene.pause();
-		this.add.text(this.sys.game.config.width / 2 - 100, this.sys.game.config.height / 2, 'Game Over', { fontSize: '36px', fill: '#fff' });
-		this.add.text(this.sys.game.config.width / 2 - 100, this.sys.game.config.height / 2 + 70, 'Final Score: ' + this.score, { fontSize: '28px', fill: '#fff' });
+		this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, 'Игра окончена',
+			{ fontSize: '28px', fill: '#fff' }
+		).setOrigin(0.5, 0.5);
+
+		this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 50, 'Счёт: ' + this.score,
+			{ fontSize: '24px', fill: '#fff' }
+		).setOrigin(0.5, 0.5);
+
+		const button = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 100, 'На главную', {
+			fontSize: '28px',
+			fill: '#fff',
+			fontStyle: 'bold',
+			backgroundColor: '#ff5044',
+			padding: { x: 16, y: 8 },
+		}).setOrigin(0.5, 0.5).setInteractive();
+
+		button.on('pointerdown', () => {
+			this.scene.start('LoadingScene');
+		});
+
+		this.physics.pause();
+		this.timerEvent.paused = true;
+		this.coinEvent.paused = true;
 	}
 
 	handleClick(pointer) {
+		if (this.coins.getLength() === 0) return
+
 		this.coins.children.iterate((coin) => {
-			if (coin.getBounds().contains(pointer.x, pointer.y)) {
+			if (coin && coin.getBounds().contains(pointer.x, pointer.y)) {
 				this.collectCoin(coin);
 			}
 		});
 	}
 
 	collectCoin(coin) {
+		const scoreAmount = coin.getData('score');
 		coin.destroy();
-		this.score += 1;
+		this.score += scoreAmount;
 		this.scoreText.setText('Score: ' + this.score);
 	}
 }
