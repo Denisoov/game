6e1d1330468301462f7b458825c645cb59710441
nd <nd@smena.space>
2024-12-08 17:15:11 +0700

    Добавил конфигурацию vue

---
+++ src/game/PhaserGame.vue
@@ -0,0 +1,13 @@
+<script setup>
+import InitPhaserGame from '../../init.js'
+import { onMounted, ref } from 'vue'
+
+const game = ref();
+
+onMounted(() => game.value = InitPhaserGame())
+
+</script>
+
+<template>
+  <div id="game-container" />
+</template>
